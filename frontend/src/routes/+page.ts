e8df7c6be311a99cf2c96ae8dcedb88bd0842aa4
Cedric Boucher <cedricjboucher@proton.me>
2026-05-23 16:21:08 -0600

    fix bug

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,8 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import { load_user } from '$lib/api';
 
-export async function load({ fetch }) {
-	const { user } = await load_user(fetch);
+export async function load() {
+	const { user } = await load_user();
 	if (user) {
 		throw redirect(302, '/masses');
 	} else {
