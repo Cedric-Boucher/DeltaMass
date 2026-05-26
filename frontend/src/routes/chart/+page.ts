2d0aaa85bef2b22ca33e02440e8dce974cbcf918
Cedric Boucher <cedricjboucher@proton.me>
2026-05-25 20:53:00 -0600

    create chart

---
+++ frontend/src/routes/chart/+page.ts
@@ -0,0 +1,14 @@
+import { redirect } from '@sveltejs/kit';
+import { getMasses, load_user } from '$lib/api';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+	const { user } = await load_user();
+
+	if (!user) {
+		throw redirect(302, '/login');
+	}
+
+	const masses = await getMasses();
+	return { masses };
+};
